4869d1e6a5088db0791de7545a25ddc382267846
Caroline Owens <carolinehowens@gmail.com>
2019-11-30 18:01:40 -0800

    Visualize percent DO for each profile using facet_wrap

--- ysi_prelim.R
+++ ysi_prelim.R
@@ -25,6 +25,11 @@ dat1 <- dat %>%
          pctDO = `Percent DO`,
          mgDO = `DO (mg/L)`) 
 
+sum(is.na(dat1$lake)) #2 entries have NA for lake
+
+dat1[374,1] <- "H1A"
+dat1[375,1] <- "H1A" #fill in the correct lake by referencing GPS locations, times
+dat1[38:43,2] <- "37 16.043" #separate profiles by lat
 
 ##############
 ### visualization
@@ -41,7 +46,43 @@ plot1 #this shows roughly 20 profiles taken over the course of the season plus s
 ##############
 
 dat2 <- dat1 %>% 
-  group_by(date + lake) %>% #group into profiles
-  slice() #select only profiles that have at least 5 measurements
-  
-  
+  group_by(date, lake, lat) %>% #group into profiles
+  filter(n()>2) #select only profiles that have at least 3 measurements
+  #and if you want to use only final 6 lakes # %>% filter(lake != "Knob")
+
+plot2 <- ggplot(data= dat2)+
+  geom_point(aes(x=z, y=pctDO))+
+  facet_wrap(~lake + date, scales = "free")+
+  theme_classic()
+
+plot2 #percent DO vs. depth for each profile 
+
+plot3 <- ggplot(data = dat2)+
+  geom_point(aes(x=z, y=mgDO))+
+  facet_wrap(~lake + date, scales = "free")+
+  theme_classic()
+
+plot3 #mg/L DO vs. depth for each profile
+
+##############
+### playing with just one sample date / lake - untangling patterns
+##############
+
+dat3 <- dat2 %>% 
+  filter(lake == "H2P", date == "3-Aug-19")
+
+plot4 <- ggplot(data = dat3)+
+  geom_point(aes(x=z, y=pctDO))+
+  facet_grid(rows=vars(lat), cols = NULL)+
+  theme_classic()
+
+plot4
+
+
+plot5 <- ggplot(data= dat2)+
+  geom_point(aes(x=z, y=pctDO))+
+  facet_wrap(~lake + lat + date, scales = "free", drop = TRUE, 
+             labeller = labeller(~lake + date, .multi_line = FALSE))+
+  theme_void()
+
+plot5
